ef3d3212c8691e02cc574e9219d82c9b5910e916
Gonzalo Fernandez <gfernand@c1r6s5.42madrid.com>
2022-02-08 13:23:41 +0100

    subo

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 11:06:46 by gfernand          #+#    #+#             */
+/*   Updated: 2022/02/07 13:59:26 by gfernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
---
+++ ft_split.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 12:53:34 by gfernand          #+#    #+#             */
+/*   Updated: 2022/02/03 15:10:25 by gfernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_count(char const *s, char c);
+
+static char		**ft_site(char **str, char const *s, char c);
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	count;
+	char	**str;
+
+	count = ft_count(s, c);
+	str = (char **)malloc(sizeof(char *) *(count + 1));
+	if (!str)
+		return (NULL);
+	ft_site(str, s, c);
+	return (str);
+}
+
+static	size_t	ft_count(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 1;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c && s[i +1])
+			count++;
+		i++;
+	}
+	if (s[0] == c || s[0] == '\0')
+		count--;
+	return (count);
+}
+
+static	char	**ft_site(char **str, char const *s, char c)
+{
+	size_t	i;
+	size_t	found;
+	size_t	count;
+	size_t	start;
+
+	i = 0;
+	found = 1;
+	count = ft_count(s, c);
+	while (found <= count)
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			i++;
+			while (s[i] != c && s[i])
+				i++;
+			str[found - 1] = ft_substr(s, start, (i - 1) - start + 1);
+			found++;
+		}
+		else
+			i++;
+	}
+	str[found - 1] = NULL;
+	return (str);
+}
