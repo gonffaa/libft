fa6c32531777e5214198c07ac869323a3fcf6d35
Gonzalo Fernandez <gfernand@c1r6s1.42madrid.com>
2022-02-25 16:32:55 +0100

    finish libft

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 12:53:34 by gfernand          #+#    #+#             */
-/*   Updated: 2022/02/03 15:10:25 by gfernand         ###   ########.fr       */
+/*   Updated: 2022/02/21 14:32:45 by gfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 static size_t	ft_count(char const *s, char c);
 
 static char		**ft_site(char **str, char const *s, char c);
+
+static char		**ft_free(char **str, size_t count);
 
 char	**ft_split(char const *s, char c)
 {
@@ -63,11 +65,13 @@ static	char	**ft_site(char **str, char const *s, char c)
 	{
 		if (s[i] != c)
 		{
-			start = i;
-			i++;
+			start = i++;
+			//i++;
 			while (s[i] != c && s[i])
 				i++;
 			str[found - 1] = ft_substr(s, start, (i - 1) - start + 1);
+			if (str[found - 1] == NULL)
+				return (ft_free(str, found - 2));
 			found++;
 		}
 		else
@@ -75,4 +79,24 @@ static	char	**ft_site(char **str, char const *s, char c)
 	}
 	str[found - 1] = NULL;
 	return (str);
+}
+
+static char     **ft_free(char **str, size_t count)
+{
+    while (count >= 0)
+    {
+        free((void *)str[count]);
+        count--;
+    }
+    free(count);
+    return (NULL);
+}
+
+int	main (void)
+{
+	char	*s;
+	char	c;
+
+	c = ",";
+	s = "hola, a , todos,,,jej";
 }
